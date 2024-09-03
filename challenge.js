74abed7ffba4e9aadb7c60834920465e1310de54
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-03 23:31:13 +0800

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let A = 14;
+let B = 20;
+
+if(A >= B){
+    console.log("The largest out of the two number is %d", A);
+}
+else if(B >= A){
+    console.log("The largest out of the two number is %d", B);
+}
+else {
+    console.log ("Something went wrong, pls retry");
+}
